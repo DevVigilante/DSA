fd062e48e613e8b4cc5a2ffbda2dcaa60ba5845c
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2026-08-09 11:37:47 +0530

    add(questionid-7): add bubble sorting solution

---
+++ sorting_algorithm/question_7.c
@@ -0,0 +1,39 @@
+/*
+ * QuestionID: 7
+ * Write a program to sort the given array of integers
+ * into ascending form using bubble sort algorithm.
+ */
+
+#include <stdio.h>
+
+void printArray(int arr[], int length) {
+  for (int i = 0; i < length; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+void sortArray(int arr[], int length) {
+  for (int i = 0; i < length; i++) {
+    for (int j = 0; j < length - i - 1; j++) {
+      int curr = arr[j];
+      int next = arr[j + 1];
+
+      if (curr > next) {
+        int temp = curr;
+        arr[j] = next;
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  printf("\n");
+}
+
+
+int main() {
+  int numbers[] = {7, 8, 5, 9, 2};
+  int length = sizeof(numbers) / sizeof(numbers[0]);
+  sortArray(numbers, length);
+  printArray(numbers, length);
+  return 0;
+}
